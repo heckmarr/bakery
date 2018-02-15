93850abc7fed7d70be76c473c18a03813870f34c
Ben Belland <benbelland@gmail.com>
2018-02-15 12:38:10 -0700

    spawn is now a chain

--- bakery.go
+++ bakery.go
@@ -11,6 +11,45 @@ import (
 	"time"
 )
 
+func Spatter(xvar int, yvar int, test_toast []flour.Bread) {
+        rand.Seed(12)
+        welcome := "WELCOME_TO_HECK"
+        wel := strings.Split(welcome, "")
+        fmt.Println(wel[0])
+        for i := 0;i < len(test_toast)/24;i++ {
+                x := rand.Intn(xvar - 1)
+                y := rand.Intn(yvar - 1)
+                slice := flour.Bread_Getter(x, y, test_toast)
+                slice.Label = string(wel[rand.Intn(len(wel)-1)])
+                test_toast = flour.Bread_Setter(x, y, test_toast, slice)
+                flour.Toast(test_toast)
+                fmt.Printf("0\nDG:>")
+                time.Sleep(75*time.Millisecond)
+                }
+}
+
+func Welcome(test_toast []flour.Bread) {
+        welcome := "WELCOME_TO_DEEGEE"
+        wel := strings.Split(welcome, "")
+        fmt.Println(wel[0])
+        for i := 0;i < len(welcome);i++ {
+//                                        DO STUFF HERE
+                slice := flour.Bread_Getter(30+i, 11, test_toast)
+                slice.Label = string(wel[i])
+                test_toast = flour.Bread_Setter(30+i, 11, test_toast, slice)
+        }
+	flour.Toast(test_toast)
+	fmt.Printf("\nDG:>")
+}
+
+func Flat(test_toast []flour.Bread) {
+        for i := range test_toast {
+                test_toast[i].Label = "_"
+        }
+        flour.Toast(test_toast)
+	fmt.Printf("\nDG:>")
+}
+
 
 func main() {
 //	toast.Toast()
@@ -23,61 +62,29 @@ func main() {
 	test_toast := flour.Dough(xvar, yvar)
 	test_toast = flour.Oven(test_toast, "BUTOOON", xvar, yvar)
 	//just toasting something
-	bread := "rye"
-	for _, v := range bread {
-		fmt.Printf(string(v))
-	}
-	fmt.Println(test_toast)
 	for {
-//		for i := range test_toast {
-//			fmt.Printf(test_toast[i].Label)
-//			if test_toast[i].Nl {
-//				fmt.Println("")
-//			}
-//		}
 		flour.Toast(test_toast)
-		fmt.Printf("0DG:>")
+		fmt.Printf("\nDG:>")
 		fmt.Scan(&input)
+
 		switch input {
 			case "spawn":
-				welcome := "WELCOME_TO_DEEGEE"
-				wel := strings.Split(welcome, "")
-				fmt.Println(wel[0])
-				for i := 0;i < len(welcome);i++ {
-//        				  DO STUFF HERE
-					slice := flour.Bread_Getter(30+i, 11, test_toast)
-					slice.Label = string(wel[i])
-					test_toast = flour.Bread_Setter(30+i, 11, test_toast, slice)
-				}
-				flour.Toast(test_toast)
-			case "matrici":
-				rand.Seed(12)
-				welcome := "WELCOME_TO_HECK"
-				wel := strings.Split(welcome, "")
-				fmt.Println(wel[0])
-				for i := 0;i < len(test_toast)/24;i++ {
-					x := rand.Intn(xvar - 1)
-					y := rand.Intn(yvar - 1)
-					slice := flour.Bread_Getter(x, y, test_toast)
-					slice.Label = string(wel[rand.Intn(len(wel)-1)])
-					test_toast = flour.Bread_Setter(x, y, test_toast, slice)
-					flour.Toast(test_toast)
-					fmt.Printf("0DG:>")
-					time.Sleep(100*time.Millisecond)
-				}
+				Flat(test_toast)
+				time.Sleep(1*time.Second)
+				Welcome(test_toast)
+				time.Sleep(1*time.Second)
+				Spatter(xvar, yvar, test_toast)
+				Flat(test_toast)
+			case "spatter":
+				Spatter(xvar, yvar, test_toast)
+			case "welcome":
+				Welcome(test_toast)
 			case "flat":
-				for i := range test_toast {
-					test_toast[i].Label = "_"
-				}
-				flour.Toast(test_toast)
+				Flat(test_toast)
 			case "exit":
 				break
 			default:
 				flour.Toast(test_toast)
 		}
 	}
-//	for _, v := range new_button {
-//		fmt.Println("X = ",v.X)
-//		fmt.Println("Y = ", v.Y)
-//	}
 }
