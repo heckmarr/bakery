05c78071b9a769947d85ce10b9760bfbcb9559fc
Ben Belland <benbelland@gmail.com>
2018-05-06 14:23:08 -0600

    localization

---
+++ poptart/poptart.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from pygame import camera as cammod
+import pygame
+import StringIO
+import os
+from PIL import Image, ImageDraw
+import asciified as asc
+import subprocess
+import sys
+import time
+import pwd
+getlogin = lambda: pwd.getpwuid(os.getuid())[0]
+username = getlogin()
+
+
+def main():
+    pygame.init()
+    cammod.init()
+    cam1 = cammod.list_cameras()
+    #print(cam1[0])
+
+    cam = cammod.Camera('/dev/video1', (1024, 768))
+    count = 0
+    cam.start()
+    ready = False
+    thumbnail = 32, 32
+    while True :
+
+        #exists = os.open("/home/turanga/1101/101/image"+str(count)+".png", os.O_CREAT)
+
+        while ready != True:
+            ready = cam.query_image()
+        if ready:
+            #print("Camera ready for picture "+ str(count))
+            path = "/home/"+username+"/go/src/localtoast.net/localtoast/bakery/poptart/101/image.png"
+            image = cam.get_image()
+            pygame.image.save(image, path)
+            pathText = asc.asciified(path)
+            ready = cam.query_image()
+            #return pathText
+        
+    cam.stop()
+
+main()
