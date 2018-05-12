ebf747b17c07a9b0b1501549e2c971355013e103
Ben Belland <benbelland@gmail.com>
2018-05-12 14:52:36 -0600

    rewrite of asciified in go

--- poptart/asciified.go
+++ poptart/asciified.go
@@ -1,9 +1,36 @@
-package poptart
+package asciified
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
+	"github.com/disintegration/imaging"
 )
 
 func main() {
-	fmt.Println("World")
+	image, err := imaging.Open("101/image.png")
+	if err != nil {
+		fmt.Println("Error opening image.")
+	}
+	asciicode := strings.Split(" -.-,-:-;-i-r-s-X-A-2-5-3-h-M-H-G-S-#-9-B-&-@", "-")
+	imageG := imaging.Grayscale(image)
+	size := 32
+	var filter imaging.ResampleFilter
+	imageG = imaging.Resize(image, size, size, filter)
+	asciiImage, err := os.Open("101/local.txt")
+	defer asciiImage.Close()
+	if err != nil {
+		fmt.Println("Error opening text file.")
+	}
+
+	for column := 0; column < size; column++ {
+		for row := 0; row < size; row++ {
+			r, g, b, _ := imageG.At(row, column).RGBA()
+			asciinum := int(r) + int(g) + int(b)/24
+
+			asciiImage.WriteString(asciicode[asciinum])
+		}
+		asciiImage.WriteString("\n")
+	}
 }
