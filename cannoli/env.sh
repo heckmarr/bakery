f2ca46b3ecdf07d471d8e4b4e95c13cdaf0c6dcb
Ben Belland <benbelland@gmail.com>
2018-05-15 18:58:41 -0600

    awww jeah, openCV time

---
+++ cannoli/env.sh
@@ -0,0 +1,3 @@
+export CGO_CPPFLAGS="-I/usr/local/include"
+export CGO_LDFLAGS="-L/usr/local/lib -lopencv_shape -lopencv_videostab -lopencv_stitching -lopencv_photo -lopencv_ml -lopencv_dnn -lopencv_objdetect -lopencv_superres -lopencv_video -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_flann -lopencv_imgproc -lopencv_core"
+
